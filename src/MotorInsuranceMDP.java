dca6436f2d1dd2f01f21a733309124bd4ccc0732
kal189 <kaleem.a@northeastern.edu>
2026-02-03 13:57:41 -0500

    Basic setup

---
+++ src/MotorInsuranceMDP.java
@@ -0,0 +1,10 @@
+package src;
+
+public class MotorInsuranceMDP {
+    public static void main(String[] args) {
+        System.out.println("Motor Insurance MDP");
+        System.out.println("Setup successful");
+    }
+
+    
+}
